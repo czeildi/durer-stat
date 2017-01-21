9015391a1f7eb0e2f765c9877fc48a31d1967795
Ildiko Czeller <czeildi@gmail.com>
2017-01-21 20:35:34 +0100

    populate SQlite db with one year's schools

--- scripts/populate_db.R
+++ scripts/populate_db.R
@@ -3,3 +3,15 @@ library("RSQLite")
 
 durer_db <- dbConnect(RSQLite::SQLite(), "data/durer-stat.sqlite")
 
+dt <- fread(file.path('data', '8H-cdfk-scores.csv'))
+
+schools <- unique(dt[, .(school = stringi::stri_trans_totitle(school), address = '', city, region)])
+
+unambiguous_schools <- schools[!str_detect(school, '\n') & !str_detect(city, ',')] %>% 
+    .[order(school)]
+
+ambiguous_schools <- schools[str_detect(school, '\n') | str_detect(city, ',')]
+
+dbWriteTable(durer_db, 'schools', unambiguous_schools, overwrite = T)
+
+dbDisconnect(durer_db)
