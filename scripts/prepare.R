d930de924ecfab1587b77a67ac36b29a70c35071
Ildiko Czeller <czeildi@gmail.com>
2017-01-21 19:53:08 +0100

    use write_csv as well

--- scripts/prepare.R
+++ scripts/prepare.R
@@ -2,7 +2,7 @@ read_csv("raw_data/9-cdfk-helyi-pontozo.csv") %>%
     translateColName() %>% 
     tidyr::separate(members, paste0('member_', 1:3), sep = '\n') %>% 
     tidyr::separate(class, paste0('class_', 1:3), sep = '\n') %>% 
-    write.csv(file.path('data', '9H-cdfk-scores.csv'), row.names = F)
+    write_csv(file.path('data', '9H-cdfk-scores.csv'))
 
 read_csv("raw_data/8-cdfk-helyi-pontozo.csv") %>% 
     translateColName() %>% 
@@ -13,4 +13,4 @@ read_csv("raw_data/8-cdfk-helyi-pontozo.csv") %>%
         class_2 = str_match(class_2, '[[:digit:]]+')[,1],
         class_3 = str_match(class_3, '[[:digit:]]+')[,1]
     )] %>% 
-    write.csv(file.path('data', '8H-cdfk-scores.csv'), row.names = F)
+    write_csv(file.path('data', '8H-cdfk-scores.csv'))
