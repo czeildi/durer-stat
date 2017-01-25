a396c20b4d7470c462d522ec625071bd4eb751c4
Ildiko Czeller <czeildi@gmail.com>
2017-01-25 21:58:49 +0100

    collect all schools from data cols

---
+++ src/collect_schools.R
@@ -0,0 +1,15 @@
+collectSchoolNames <- function() {
+  school_names <- character(0)
+  walk(
+      list.files('raw_data', full.names = TRUE),
+      ~ {
+          suppressMessages(dt <- read_csv(.))
+          if ('Iskola' %in% names(dt)) {
+              school_names <<- unique(c(school_names, dt$Iskola))
+          } else {
+              print(.)
+          }
+      }
+  )
+  school_names
+}
