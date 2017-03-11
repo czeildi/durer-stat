e7f68259ee01ce65173523cfdf607c3b846c7a38
Ildiko Czeller <czeildi@gmail.com>
2017-03-11 19:34:28 +0100

    redownload data woth some unnecessary columns deleted

--- scripts/handle_google_sheets.R
+++ scripts/handle_google_sheets.R
@@ -12,17 +12,18 @@ durer_eredmenyek <- sheets[
         grepl('[[:digit:]]{1,2}[HDLK][_A-Z]{0,5}$', sheet_title)
     ]
 
-# walk(
-#     durer_eredmenyek$sheet_title,
-#     ~ {
-#         registered_sheet_obj <- gs_key(durer_eredmenyek[sheet_title == ., sheet_key])
-#         gs_download(
-#             registered_sheet_obj,
-#             ws = 'transformed',
-#             to = file.path('raw_data', str_c(., '.csv'))
-#         )
-#     }
-# )
+walk(
+    durer_eredmenyek$sheet_title,
+    ~ {
+        registered_sheet_obj <- gs_key(durer_eredmenyek[sheet_title == ., sheet_key])
+        gs_download(
+            registered_sheet_obj,
+            ws = 'transformed_w_unique_scools',
+            to = file.path('raw_data', str_c(., '.csv')),
+            overwrite = TRUE
+        )
+    }
+)
 # 
 # walk(
 #     durer_eredmenyek$sheet_title,
