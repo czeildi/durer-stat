18d2cbd62b2a495160662ba440e8997f00c94be5
Ildiko Czeller <czeildi@gmail.com>
2017-03-11 19:13:53 +0100

    we can also upload transformed files to gdrive

--- scripts/handle_google_sheets.R
+++ scripts/handle_google_sheets.R
@@ -12,14 +12,26 @@ durer_eredmenyek <- sheets[
         grepl('[[:digit:]]{1,2}[HDLK][_A-Z]{0,5}$', sheet_title)
     ]
 
-walk(
-    durer_eredmenyek$sheet_title,
-    ~ {
-        registered_sheet_obj <- gs_key(durer_eredmenyek[sheet_title == ., sheet_key])
-        gs_download(
-            registered_sheet_obj,
-            ws = 'transformed',
-            to = file.path('raw_data', str_c(., '.csv'))
-        )
-    }
-)
+# walk(
+#     durer_eredmenyek$sheet_title,
+#     ~ {
+#         registered_sheet_obj <- gs_key(durer_eredmenyek[sheet_title == ., sheet_key])
+#         gs_download(
+#             registered_sheet_obj,
+#             ws = 'transformed',
+#             to = file.path('raw_data', str_c(., '.csv'))
+#         )
+#     }
+# )
+# 
+# walk(
+#     durer_eredmenyek$sheet_title,
+#     ~ {
+#         registered_sheet_obj <- gs_key(durer_eredmenyek[sheet_title == ., sheet_key])
+#         gs_ws_new(
+#             registered_sheet_obj,
+#             ws_title = 'transformed_w_unique_scools',
+#             input = read_csv(file.path('raw_data', str_c(., '.csv')))
+#         )
+#     }
+# )
